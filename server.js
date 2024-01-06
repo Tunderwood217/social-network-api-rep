dbc99b9af774dc68b0fcf64894b4518569a28b39
Tiffani Underwood <tdunderwood217@gmail.com>
2024-01-06 14:39:32 -0500

    added post route

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const db = require('./main/config/connection');
+const routes = require('./main/routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+});
